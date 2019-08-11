10ac2fb97b1c13a48ee5bd613a13d8205a417551
lanshan <yangjr@tuya.com>
2019-08-11 11:53:53 +0800

    1154

---
+++ main/game/game_19_8_11/game_1154.go
@@ -0,0 +1,36 @@
+package game_19_8_11
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ordinalOfDate(date string) int {
+	if len(date) < 10 {
+		return 0
+	}
+	//month := []string{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}
+	days := []int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	days1 := []int{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	split := strings.Split(date, "-")
+	i1, e := strconv.Atoi(split[0])
+	i2, e2 := strconv.Atoi(split[1])
+	i3, e1 := strconv.Atoi(split[2])
+	if e != nil || e1 != nil || e2 != nil {
+		return 0
+	}
+	var count int
+	if i1%400 == 0 || (i1%4 == 0 && i1%100 != 0) {
+		for i := 1; i < i2; i++ {
+			count += days1[i]
+		}
+		count += i3
+	} else {
+		for i := 1; i < i2; i++ {
+			count += days[i]
+		}
+		count += i3
+	}
+	return count
+
+}
