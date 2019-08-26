3bf184c01b7b4108044641d6f7a8aedbe04202b4
lanshan <yangjr@tuya.com>
2019-08-27 00:05:24 +0800

    122

--- main/leetcode201908/leetcode_121.go
+++ main/leetcode201908/leetcode_121.go
@@ -1,18 +1,18 @@
 package main
 
-func maxProfit(prices []int) int {
+func maxProfit1(prices []int) int {
 	max := -1
 	for i := 0; i < len(prices); i++ {
 		for j := i + 1; j < len(prices); j++ {
 			if prices[j] > prices[i] {
-				max = maxNumProfit(max, prices[j]-prices[i])
+				max = maxNumProfit1(max, prices[j]-prices[i])
 			}
 		}
 	}
 	return max
 }
 
-func maxNumProfit(a, b int) int {
+func maxNumProfit1(a, b int) int {
 	if a > b {
 		return a
 	}
---
+++ main/leetcode201908/leetcode_122.go
@@ -0,0 +1,12 @@
+package main
+
+func maxProfit(prices []int) int {
+
+	price := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			price += prices[i] - prices[i-1]
+		}
+	}
+	return price
+}
